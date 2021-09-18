bc5e5eb664153051d6fc791095d770851fe4a780
Uniman <uniman.malaysia@gmail.com>
2021-09-18 17:16:33 +0430

    Media directory is added

--- muzibeat/administrator/models.py
+++ muzibeat/administrator/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+    )
+
+    title = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='PostImages/')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+
+
+
+
+
+
+# class concertModel(models.Model):
+#     Name = models.CharField(max_length=40)
+#     SingerName = models.CharField(max_length=40)
+#     Lenght = models.IntegerField()
+#     Poster = models.ImageField(upload_to='concertImage/',null=True)
+#
+#     def __str__(self):
+#         return self.SingerName
--- muzibeat/muzibeat/settings.py
+++ muzibeat/muzibeat/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'administrator',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
