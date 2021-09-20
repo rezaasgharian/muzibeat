e83f2f8bbf33c742896a020616cfb4f701ceeeb2
Uniman <uniman.malaysia@gmail.com>
2021-09-20 11:39:48 +0430

    Payments app is added

--- muzibeat/payments/admin.py
+++ muzibeat/payments/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Payment
-admin.site.register(Payment)
+
 
 # Register your models here.
+class paymentAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'product_id', 'price', 'ref', 'status', 'date')
+    list_filter = ('date', 'status')
+    search_fields = ('title', 'description')
+    ordering = ['status', 'date']
+
+admin.site.register(Payment,paymentAdmin)
