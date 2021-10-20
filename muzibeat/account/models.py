27bdd549074a11d8272f895c019d8f6eb24270e4
mohammad0x <mohammadtaghdir13@gmail.com>
2021-10-20 12:47:32 +0330

    profile is completed.28/7/1400

--- muzibeat/account/models.py
+++ muzibeat/account/models.py
@@ -59,7 +59,7 @@ class Profile(models.Model):
         ('s', 'Sad'),
         ('b', 'Busy'),
         ('a', 'Available'),
-        ('m', 'Motivate'),
+        (',', 'Motivate'),
         ('l', 'Love'),
     )
     EXPERT_CHOICES =(
@@ -72,8 +72,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     desc = models.TextField(blank=True)
     image = models.ImageField(upload_to='avatars/', blank=True)
-    status = models.CharField(max_length=5, choices=STATUS_CHOICES)
-    expert = models.CharField(max_length=3, choices=EXPERT_CHOICES , default=3)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    expert = models.CharField(max_length=10, choices=EXPERT_CHOICES , default=3)
     nationality = CountryField()
 
 def save_profile_user(sender, **kwargs):
@@ -88,11 +88,9 @@ class Post_user(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)
     title = models.CharField(max_length=40)
     description = models.TextField()
-    avatar = models.ImageField(null=True, blank=True)
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
 
 class Images(models.Model):
     post = models.ForeignKey(Post_user, on_delete=models.CASCADE,null=True, blank=True)
